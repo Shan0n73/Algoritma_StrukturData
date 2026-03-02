2ecbe2c44518c046e1956927824e155e710e679d
Shan0n73 <agrisyandimaraja@gmail.com>
2026-03-02 16:22:58 +0700

    Percobaan 1

---
+++ js2/Mahasiswa01.java
@@ -0,0 +1,40 @@
+public class Mahasiswa01 {
+
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        ipk = ipkBaru;
+        if (0.0 <= ipk && ipk <= 4.0) {
+            System.out.println("IPK valid");
+        } else {
+            System.out.println("IPK tidak valid");
+        }
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return " Kinerja Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja Baik";
+        } else if (ipk >= 2.5) {
+            return "Kinerja Cukup";
+        } else {
+            return "KinerjaKurang";
+        }
+    }
+
+}
