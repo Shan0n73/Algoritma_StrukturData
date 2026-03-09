9a2ae4cdbf1641d74cd059442920abc474f242cc
Shan0n73 <agrisyandimaraja@gmail.com>
2026-03-09 18:20:56 +0700

    Percobaan 3 Modifikasi 2

--- js3/MataKuliah01.java
+++ js3/MataKuliah01.java
@@ -10,4 +10,18 @@ public class MataKuliah01 {
         this.sks = sks;
         this.jumlahJam = jumlahJam;
     }
+    public void tambahData(String code, String nama, int sks, int jumlahJam){
+        this.code = code;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+    void cetakinfo(int i){
+        System.out.println("Data Matakuliah ke-" + (i + 1) + " : ");
+            System.out.println("KODE\t\t:"+code);
+            System.out.println("NAMA\t\t:"+nama);
+            System.out.println("SKS\t\t:"+sks);
+            System.out.println("JUMLAH JAM\t: "+jumlahJam);
+            System.out.println("------------------------------------------");
+    }
 }
