412583e63fea531bbc65211488d25bc6d2cf062f
Shan0n73 <agrisyandimaraja@gmail.com>
2026-03-16 21:06:01 +0700

    Percobaan 2

---
+++ js5/MainPangkat.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class MainPangkat {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen : ");
+        int elemen = sc.nextInt();
+
+        Pangkat[] png = new Pangkat[elemen];
+        for (int i = 0; i < elemen; i++) {
+            System.out.print("Masukkan nilai basis elemen ke-" + i + " : ");
+            int basis = sc.nextInt();
+            System.out.print("Masukkan nilai Pangkat elemen ke-" + i + " : ");
+            int pangkat = sc.nextInt();
+            png[i] = new Pangkat(basis, pangkat);
+        }
+        System.out.println("HASIL PANGKAT BRUTE FORCE : ");
+        for (Pangkat p : png) {
+            System.out.println(p.nilai + "^" + p.pangkat + " : " + p.pangkatBF(p.nilai, p.pangkat));
+        }
+        System.out.println("HASIL PANGKAT DIVIDE CONQUER : ");
+        for (Pangkat p : png) {
+            System.out.println(p.nilai + "^" + p.pangkat + " : " + p.pangkatDC(p.nilai, p.pangkat));
+        }
+    }
+}
